ba65c67ec344d8d3ff26b2fd1d2df47f11832674
maximustdie <maximustdie@yandex.ru>
2023-11-13 10:17:18 +0300

    added get_all_books_pagination_limit_offset

--- src/api/http/routers/v1/book.py
+++ src/api/http/routers/v1/book.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from abstractions.decorators.transactional import transactional
@@ -17,3 +18,12 @@ async def create_book(data: BookCreateDTO, session: AsyncSession = Depends(get_c
     await session.flush()
     await session.refresh(book)
     return BookDTO.model_validate(book)
+
+
+@router.get("/books")
+@transactional
+async def get_all_books_pagination_limit_offset(session: AsyncSession = Depends(get_current_session)):
+    stmt = select(BookModel)
+    result = (await session.execute(stmt)).scalars().all()
+    return [BookDTO(**book.as_dict()) for book in result]
+
